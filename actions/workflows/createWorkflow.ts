6672522bad91dc52d8d63ec16b6414be95e6c1a7
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-11 23:52:07 +0530

    few bugs resolved

--- actions/workflows/createWorkflow.ts
+++ actions/workflows/createWorkflow.ts
@@ -1,40 +1,38 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
-import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow";
+import {
+  createWorkflowSchema,
+  createWorkflowSchemaType,
+} from "@/schema/workflow";
 import { WorkflowStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
+export async function CreateWorkflow(form: createWorkflowSchemaType) {
+  const { success, data } = createWorkflowSchema.safeParse(form);
+  if (!success) {
+    throw new Error("invalid form data");
+  }
 
-export async function CreateWorkflow(form: createWorkflowSchemaType){
-    const {success, data} = createWorkflowSchema.safeParse(form);
-    if(!success){
-        throw new Error("invalid form data"); 
-    }
+  const { userId } = auth();
 
-    const {userId} = auth();
+  if (!userId) {
+    throw new Error("unauthorized");
+  }
 
-    if(!userId){
-        throw new Error("unauthorized");
-    }
+  const result = await prisma.workflow.create({
+    data: {
+      userId,
+      status: WorkflowStatus.DRAFT,
+      definition: "TODO",
+      ...data,
+      description: "abc",
+    },
+  });
 
-    try {
-        const result = await prisma.workflow.create({
-            data: {
-                userId,
-                status: WorkflowStatus.DRAFT,
-                definition: "TODO", 
-                ...data,
-            },
-        });
-    
-        if (!result) {
-          throw new Error("Failed to create workflow");
-        }
-        redirect(`/workflow/editor/${result.id}`);
-      } catch (err) {
-        console.error("Error creating workflow:", err);
-        throw new Error("Failed to create workflow");
-      }
+  if (!result) {
+    throw new Error("Failed to create workflow");
+  }
+  redirect(`/workflow/editor/${result.id}`);
 }
--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -14,7 +14,7 @@ function page() {
           <h1 className="text-3xl font-bold">Workflows</h1>
           <p className="text-muted-foreground">Manage your workflows</p>
         </div>
-        <CreateWorkFlowDialog triggerText="Create workflow"/>
+        <CreateWorkFlowDialog triggerText="Create workflow" />
       </div>
       <div className="h-full py-6">
         <Suspense fallback={<UserWorkflowsSkeleton />}>
@@ -36,49 +36,44 @@ function UserWorkflowsSkeleton() {
 }
 
 async function UserWorkflows() {
-
   const workflows = await getWorkflowsForUser();
 
   if (!workflows) {
-
     return (
       <Alert variant={"destructive"}>
         <AlertCircle className="w-4 h-4" />
         <AlertTitle>Error</AlertTitle>
         <AlertDescription>
-            Something went wrong, Please try again later
+          Something went wrong, Please try again later
         </AlertDescription>
       </Alert>
     );
-    
   }
 
-  if(workflows.length === 0){
+  if (workflows.length === 0) {
     return (
-        <div className="flex flex-col gap-4 h-full items-center justify-center">
-            <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
-                <InboxIcon size={40} className="stroke-primary "/>
-            </div>
-            <div className="flex flex-col gap-1 text-center">
-                <p className="font-bold">No workflows yet</p>
-                <p className="text-sm text-muted-foreground">
-                    Click the button below to create your first workflow
-                </p>
-            </div>
-
-            <CreateWorkFlowDialog triggerText="Create your first workflow"/>
+      <div className="flex flex-col gap-4 h-full items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon size={40} className="stroke-primary " />
         </div>
-    )
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflows yet</p>
+          <p className="text-sm text-muted-foreground">
+            Click the button below to create your first workflow
+          </p>
+        </div>
+
+        <CreateWorkFlowDialog triggerText="Create your first workflow" />
+      </div>
+    );
   }
 
   return (
-      <div className="grid grid-cols-1 gap-4">
-        {workflows.map((workflow,index) => (
-          <WorkflowCard key={workflow.id} workflow={workflow}/>
-        ))}
-
-      </div>
-  
+    <div className="grid grid-cols-1 gap-4">
+      {workflows.map((workflow, index) => (
+        <WorkflowCard key={workflow.id} workflow={workflow} />
+      ))}
+    </div>
   );
 }
 
