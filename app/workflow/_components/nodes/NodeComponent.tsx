1badce43286708cf49308f24d1af01cf53e0fde6
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-14 22:52:46 +0530

    node component

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -3,14 +3,22 @@ import NodeCard from "./NodeCard";
 import { NodeProps } from "@xyflow/react";
 import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+import { NodeInput, NodeInputs } from "./NodeInputs";
 
 const NodeComponent = memo((props: NodeProps) => {
 
     const nodeData  = props.data as AppNodeData; 
+    const task = TaskRegistry[nodeData.type];
 
     return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected} >
         <NodeHeader taskType={nodeData.type}/>
+        <NodeInputs>
+            {task.inputs.map((input)=>(
+                <NodeInput key={input.name} input={input} nodeId={props.id}/>
+            ))}
+        </NodeInputs>
     </NodeCard>
     );
 });
