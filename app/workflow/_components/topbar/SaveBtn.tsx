37e7d23d95a5cee11d7bc8bd52778023d6a2474a
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-16 17:34:41 +0530

    .

--- app/workflow/_components/topbar/SaveBtn.tsx
+++ app/workflow/_components/topbar/SaveBtn.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { Button } from '@/components/ui/button';
+import { useReactFlow } from '@xyflow/react';
 import { CheckIcon } from 'lucide-react';
 import React from 'react'
 
 function SaveBtn() {
+    const {toObject} = useReactFlow();
   return (
     <div>
       <Button 
         variant={"outline"} 
         className='flex items-center gap-2'
         onClick={()=> {
-            alert("TODO");
+      
+            console.log("@FLOw",toObject)
         }}
         >
             <CheckIcon size={16} className='stroke-green-400'/>
