1890fd74542dd3d59dc2bce4c991f7c18e04ad48
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-17 23:35:13 +0530

    save btn

--- app/workflow/_components/topbar/SaveBtn.tsx
+++ app/workflow/_components/topbar/SaveBtn.tsx
@@ -1,11 +1,24 @@
 "use client";
+import { UpdateWorkflow } from '@/actions/workflows/updateWorkflow';
 import { Button } from '@/components/ui/button';
+import { useMutation } from '@tanstack/react-query';
 import { useReactFlow } from '@xyflow/react';
 import { CheckIcon } from 'lucide-react';
 import React from 'react'
+import { toast } from 'sonner';
 
 function SaveBtn({workflowId}:{workflowId:string}) {
     const {toObject} = useReactFlow();
+
+    const saveMutation = useMutation({
+      mutationFn: UpdateWorkflow,
+      onSuccess: () => {
+        toast.success("Flow saved successfully", {id: "save-workflow"});
+      },
+      onError: ()=> {
+        toast.error("Flow saved successfully", {id: "save-workflow"});
+      }
+    });
 
   return (
     <div>
@@ -13,8 +26,12 @@ function SaveBtn({workflowId}:{workflowId:string}) {
         variant={"outline"} 
         className='flex items-center gap-2'
         onClick={()=> {
-      
-            console.log("@FLOw",toObject)
+            const workflowDefinition = JSON.stringify(toObject());
+            toast.loading("Saving workflow...", {id: "save-workflow"});
+            saveMutation.mutate({
+              id: workflowId,
+              definition: workflowDefinition,
+            });
         }}
         >
             <CheckIcon size={16} className='stroke-green-400'/>
