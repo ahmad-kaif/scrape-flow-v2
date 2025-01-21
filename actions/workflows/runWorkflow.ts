f50d186017253a251de1343d0f54063946361c06
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-21 23:56:59 +0530

    db updated

--- actions/workflows/runWorkflow.ts
+++ actions/workflows/runWorkflow.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import prisma from "@/lib/prisma";
+import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
 import { WorkflowExecutionPlan } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 
@@ -25,9 +26,30 @@ export async function RunWorkflow(form: {
         },
     });
 
+    if(!workflow){
+        throw new Error("workflow not defined");
+    }
+
     let executionPlan : WorkflowExecutionPlan;
 
     if(!flowDefinition){
         throw new Error("flow definition is not defined");
     }
+
+    const flow = JSON.parse(flowDefinition);
+
+    const result = FlowToExecutionPlan(flow.nodes,flow.edges);
+
+    if(result.error){
+        throw new Error("flow definition not valid");
+    }
+    if(!result.executionPlan){
+        throw new Error("no execution plan genrated");
+    }
+
+    executionPlan = result.executionPlan;
+    console.log("execution plan", executionPlan)
+
+
+
 }
--- lib/workflow/executionPlan.ts
+++ lib/workflow/executionPlan.ts
@@ -1,6 +1,6 @@
 import { AppNode, AppNodeMissingInputs } from "@/types/appNode";
 import { WorkflowExecutionPlan ,WorkflowExecutionPlanPhase,} from "@/types/workflow";
-import { Edge, getIncomers } from "@xyflow/react";
+import { Edge} from "@xyflow/react";
 import { TaskRegistry } from "./task/registry";
 import { error } from "console";
 
@@ -148,4 +148,20 @@ function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {
   }
 
   return invalidInputs;
+}
+
+
+function getIncomers(node:AppNode, nodes:AppNode[], edges: Edge[]){
+  if(!node.id){
+    return [];
+  }
+
+  const incomerIds = new Set();
+  edges.forEach((edge)=>{
+    if(edge.target === node.id){
+      incomerIds.add(edge.source)
+    }
+  })
+
+  return nodes.filter((n) => incomerIds.has(n.id) )
 }
