110cf493273674403f96ea48ffee01c95dd62b7f
Ahmad Kaif <heyahmad611@gmail.com>
2025-03-17 16:26:50 +0530

    theme changes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,13 +10,15 @@ import React from 'react';
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex h-screen'>
+
       <DesktopSidebar />
+
       <div className='flex flex-col flex-1 min-h-screen'>
         <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
           <BreadcrumbHeader />
           <div className='gap-1 flex items-center'>
             <ModeToggle />
-            
+          
                 <SignedIn>
                   <UserButton />
                 </SignedIn>
@@ -30,6 +32,7 @@ function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </div>
       </div>
+
     </div>
   );
 }
